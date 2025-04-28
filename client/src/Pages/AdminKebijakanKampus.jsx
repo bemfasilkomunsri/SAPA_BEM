51bb55287c04702229e33bce66ed21e085237fd9
Nabila Ayu Talita <nabilaayutalitaa@gmail.com>
2025-04-28 19:24:36 +0700

    fix: Akses Admin dashboard

--- client/src/Pages/AdminKebijakanKampus.jsx
+++ client/src/Pages/AdminKebijakanKampus.jsx
@@ -1,7 +1,30 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getMe } from "../features/AuthSlice";
 import AdminDetail from "../Component/AdminDetail/kebijakan_kampus";
 import NavbarPages from "../Component/NavbarPages";
 
 const AdminKebijakanKampus = () => {
+
+  // Dispatch dan Navigate dari React Router
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {isError} = useSelector((state) => state.auth);
+  
+  // Cek user login
+  useEffect(()=>{
+    dispatch(getMe());
+  }, [dispatch]);
+  
+  // Kalo user tidak login, redirect ke page login
+  useEffect(()=>{
+    if(isError){
+        navigate("/admin");
+    }
+  }, [isError, navigate]);
+
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:p-0 p-0">
       <div className="w-full ">
--- client/src/Pages/AdminPage.jsx
+++ client/src/Pages/AdminPage.jsx
@@ -1,8 +1,29 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getMe } from "../features/AuthSlice";
 import Admin from "../Component/AdminDashboard";
 import NavbarPages from "../Component/NavbarPages"
 
 
 const AdminPage = () => {
+  // Dispatch dan Navigate dari React Router
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {isError} = useSelector((state) => state.auth);
+  
+  // Cek user login
+  useEffect(()=>{
+    dispatch(getMe());
+  }, [dispatch]);
+  
+  // Kalo user tidak login, redirect ke page login
+  useEffect(()=>{
+    if(isError){
+        navigate("/admin");
+    }
+  }, [isError, navigate]);
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:p-0 p-0">
       <div className="w-full ">
